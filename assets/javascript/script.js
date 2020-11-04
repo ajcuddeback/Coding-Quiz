21c733b5a174234b30e3a69dd5a9ac103a2b2bd6
ajcudd <ajcuddeback@gmail.com>
2020-11-04 18:07:36 -0500

    created template and pseudocode

---
+++ assets/javascript/script.js
@@ -0,0 +1,21 @@
+//create all global variables
+
+//create a start quiz function that will link to a button
+
+//seperate each Question numbers in to a array?
+
+//call upon each array whenever one of the answers are clicked
+
+//all multiple choice questions must be buttons
+
+//create a time function that will count down from 60 to 0 and will have an iteration of 1 second
+
+//time function will go down 10 seconds every wrong answer
+
+// time function will stop whenever all questions are answered or when it hits zero
+
+// quiz will end when all multiple choice answered are answered or player runs out of time
+
+//Add an input for player to put their initials and record it in local memeory along side their high score
+ 
+//print out their initials and high score on last page

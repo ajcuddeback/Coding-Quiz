c54000073d00295fc482d89945658047b162f6db
ajcudd <ajcuddeback@gmail.com>
2020-11-04 19:53:08 -0500

    added main content elements to DOM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,27 @@
 //create all global variables
-
+var mainContent = document.querySelector("#main-content");
 //create a start quiz function that will link to a button
+
+//welcome varibales
+var welcomeHeading = document.createElement("h1");
+var welcomeRules = document.createElement("h3");
+var startButton = document.createElement("button")
+
+//styles for variables
+welcomeHeading.className = "welcome-heading";
+welcomeRules.className = "welcome-rules";
+startButton.className = "start-button";
+
+//text-content for welcome items
+welcomeHeading.textContent = "Welcome to quiz";
+welcomeRules.textContent = "Press start to begin";
+startButton.textContent = "Start";
+
+
+// Append Welcome items
+mainContent.appendChild(welcomeHeading);
+mainContent.appendChild(welcomeRules);
+mainContent.appendChild(startButton);
 
 
 //seperate each Question numbers in to a array?

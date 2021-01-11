3fbbf1bbaf275cc4494601f93c17b08cdf9f7bbd
ajcudd <ajcuddeback@gmail.com>
2021-01-11 12:29:48 -0500

    adding post request to server

--- routes/apiRoutes/scoreRoutes.js
+++ routes/apiRoutes/scoreRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const fs = require('fs');
 const { scores } = require('../../data/scores.json');
+const { createNewScore } = require('../../lib/score')
 
 router.get('/scores', (req, res) => {
     res.json(scores)
     console.log(scores)
 });
+
+router.post('/scores', (req, res) => {
+    const score = createNewScore(req.body, scores);
+    res.json(score);
+})
 
 module.exports = router;
